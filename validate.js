f5688c62774d29acfe2b4228a9ae359fae7b3fc7
Moro Musharafa <mushabena18@gmail.com>
2025-01-06 23:50:07 +0000

    Update validate.js

--- validate.js
+++ validate.js
@@ -1,16 +1,2 @@
-function validateForm() {
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    
-    if (name.length < 5) {
-        alert("Name must be at least 5 characters long.");
-        return false; // Prevent form submission
-    }
-    
-    if (!email.includes("@") || email.length < 6) {
-        alert("Please enter a valid email address.");
-        return false; // Prevent form submission
-    }
-    
-    return true; // Allow form submission
-}
+
+
